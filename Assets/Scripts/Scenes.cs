10563a2a65dc55a6f23e5845984c10f7339c2b5d
Caetano <caetanofreitas72@gmail.com>
2022-05-16 20:13:36 -0300

    feat: creating start page and starting options

--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assets.Scripts {
-    internal static class Scenes {
-        public static readonly string WIN = "Win";
-        public static readonly string LOSE = "Lose";
-        public static readonly string LEVEL = "Level";
-    }
+namespace Assets.Scripts
+{
+  internal static class Scenes
+  {
+    public static readonly string WIN = "Win";
+    public static readonly string LOSE = "Lose";
+    public static readonly string LEVEL = "Level";
+    public static readonly string START = "StartMenu";
+    public static readonly string OPTIONS = "Options";
+  }
 }
