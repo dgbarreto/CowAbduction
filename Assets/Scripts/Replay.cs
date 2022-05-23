55a2ff9c6b1fa1a6d0f54675b47363c498914aaa
Caetano <caetanofreitas72@gmail.com>
2022-05-23 18:50:09 -0300

    feat: fixing win screen layout

--- Assets/Scripts/Replay.cs
+++ Assets/Scripts/Replay.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Replay : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-    }
-
     public void ReplayGame() {
         SceneManager.LoadScene(Scenes.START);
     }
