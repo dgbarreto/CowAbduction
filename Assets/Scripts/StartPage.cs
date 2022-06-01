f2c9009e2e2ab135a6cb928765f36f67bfb9abb4
Caetano <caetanofreitas72@gmail.com>
2022-05-31 21:25:14 -0300

    feat: adding tutorial

--- Assets/Scripts/StartPage.cs
+++ Assets/Scripts/StartPage.cs
@@ -2,10 +2,40 @@ using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class StartPage : MonoBehaviour
 {
+  public GameObject videoPlayer;
+
+  void Start()
+  {
+    videoPlayer.transform.GetChild(0).GetComponent<VideoPlayer>().loopPointReached += CheckOver;
+  }
+
+  void CheckOver(UnityEngine.Video.VideoPlayer vp)
+  {
+    StartCoroutine(CloseVideo());
+
+  }
+
+  IEnumerator CloseVideo()
+  {
+    yield return new WaitForSeconds(1);
+    videoPlayer.SetActive(false);
+  }
+
+
+
+  public void ShowTutorial()
+  {
+    videoPlayer.SetActive(true);
+    VideoPlayer video = videoPlayer.transform.GetChild(0).GetComponent<VideoPlayer>();
+    video.Play();
+  }
+
+
   public void QuitGame()
   {
     Application.Quit();
